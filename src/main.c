636dcc66e9545e4ca26cc9cfe9ec73737ba4a55c
ignurof <ignurof@ignurof.com>
2023-07-11 21:49:04 +0200

    more clear variable names

--- src/main.c
+++ src/main.c
@@ -10,19 +10,19 @@ int main(void)
 	// const char APP_VERSION[6] = "v0.0.1";
 
 	int should_app_close = 0;
-	int age_input = 0;
+	int date_birth = 0;
 
 	printf("age-since-birth %s\n\n", APP_VERSION);
 
 	while(should_app_close == 0)
 	{
 		printf("Enter the date of birth in this format: 19990101\n");
-		printf("Age: ");
+		printf("Date of birth: ");
 		// This creates a stray newline for the next input in the stream
 		// all scanf after this should have a blank space before the conversion specifier in the format string
 		// https://stackoverflow.com/questions/13542055/how-to-do-scanf-for-single-char-in-c
-		int age_result = scanf("%i", &age_input);
-		printf("age_result: %i\n", age_result);
+		int date_birth_input = scanf("%i", &date_birth);
+		printf("date_birth_input: %i\n", date_birth_input);
 
 		// TODO: this should return a human-readable struct that can be used to print out all the values we might want
 		// ie; seconds since birth, hours, days etc etc
@@ -42,18 +42,18 @@ int main(void)
 
 void handle_app_exit(int *should_app_close)
 {
-	char close_input = 'n';
+	char close_option = 'n';
 	
 	printf("Do you want to go again? (y)es / (n)o\n");
-	int close_result = scanf(" %c", &close_input);
-	printf("close_result: %i\n", close_result);
+	int close_option_input = scanf(" %c", &close_option);
+	printf("close_option_input: %i\n", close_option_input);
 	
-	if(close_input == 'n')
+	if(close_option == 'n')
 	{
 		// Here I need to dereference the pointer, 
 		// to change the value located at the adress which the pointer contains
 		*should_app_close = 1;
 	}
 	
-	printf("restarting...\n");
+	printf("Restarting...\n");
 }
