067af47f9c7ae3ff43c699e89f1722fb1e602004
ignurof <ignurof@ignurof.com>
2023-07-11 21:34:51 +0200

    handle_app_exit

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "time_conversion.h"
 
+#define APP_VERSION "v0.0.1"
+
+void handle_app_exit(int *);
+
 int main(void)
 {
-	const char APP_VERSION[6] = "v0.0.1";
+	// const char APP_VERSION[6] = "v0.0.1";
 
 	int should_app_close = 0;
-	char close_input = 'n';
 	int age_input = 0;
 
 	printf("age-since-birth %s\n\n", APP_VERSION);
@@ -16,27 +19,41 @@ int main(void)
 		printf("Enter the date of birth in this format: 19990101\n");
 		printf("Age: ");
 		// This creates a stray newline for the next input in the stream
-		// see below comment-block for explanation
+		// all scanf after this should have a blank space before the conversion specifier in the format string
+		// https://stackoverflow.com/questions/13542055/how-to-do-scanf-for-single-char-in-c
 		int age_result = scanf("%i", &age_input);
 		printf("age_result: %i\n", age_result);
 
+		// TODO: this should return a human-readable struct that can be used to print out all the values we might want
+		// ie; seconds since birth, hours, days etc etc
 		handle_time_conversion();
 
-		printf("Do you want to go again? (y)es / (n)o\n");
-		/*
-		 * https://stackoverflow.com/questions/13542055/how-to-do-scanf-for-single-char-in-c
-		 The %c conversion specifier won't automatically skip any leading whitespace, so if there's a stray newline in the input stream (from a previous entry, for example) the scanf call will consume it immediately.
-		 One way around the problem is to put a blank space before the conversion specifier in the format string:
-		 */
-		int close_result = scanf(" %c", &close_input);
-		printf("close_result: %i\n", close_result);
-		if(close_input == 'n')
-		{
-			should_app_close = 1;
-		}
-		printf("restarting...\n");
+		// C pass variables by value, so to change a variable inside of the function scope
+		// I need to pass the adress of the variable, and a adress is a pointer variable
+		// That means the function should take a int pointer as we are trying to change the value of an int
+		// handle_app_exit(int *);
+		handle_app_exit(&should_app_close);
 	}
+	
 	printf("Exiting program...\n");
 
 	return 0;
+}
+
+void handle_app_exit(int *should_app_close)
+{
+	char close_input = 'n';
+	
+	printf("Do you want to go again? (y)es / (n)o\n");
+	int close_result = scanf(" %c", &close_input);
+	printf("close_result: %i\n", close_result);
+	
+	if(close_input == 'n')
+	{
+		// Here I need to dereference the pointer, 
+		// to change the value located at the adress which the pointer contains
+		*should_app_close = 1;
+	}
+	
+	printf("restarting...\n");
 }
