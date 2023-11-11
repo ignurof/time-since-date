fded28fbc66c6365d12ff38dc26908fc16dfce1c
ignurof <ignurof@ignurof.com>
2023-11-11 11:50:42 +0100

    format

--- src/main.c
+++ src/main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include "../../thyme/thyme.h"
 
-// Program should handle frontend (input, output)
-// Library should handle backend (calculations)
-
-// This creates a stray newline for the next input in the stream
-// all scanf after this should have a blank space before the conversion specifier in the format string
-// https://stackoverflow.com/questions/13542055/how-to-do-scanf-for-single-char-in-c
 int main(void)
 {
 	struct tsd time_since_date = get_time_since_date();
