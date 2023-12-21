61abe4ac93c5d2a414f53a84c696eb713f59b5cc
ignurof <ignurof@ignurof.com>
2023-12-21 21:47:07 +0100

    naming

--- src/main.c
+++ src/main.c
@@ -3,26 +3,26 @@
 
 int main(void)
 {
-	struct dti date_time_input = {2023, 1, 5, 9, 42, 0};
+	struct date_time_input_t dti = {2023, 1, 5, 9, 42, 0};
 
-	struct tsd time_since_date = get_time_since_date(date_time_input);
+	struct time_since_date_t tsd = get_time_since_date(dti);
 
 	printf("%i years, %i months, %i days, %i hours, %i minutes, %i seconds\n", 
-			time_since_date.years_since_date, 
-			time_since_date.current_month_date_count, 
-			time_since_date.current_day_date_count, 
-			time_since_date.current_hour_date_count, 
-			time_since_date.current_minute_date_count, 
-			time_since_date.current_second_date_count
+			tsd.years_since_date, 
+			tsd.current_month_date_count, 
+			tsd.current_day_date_count, 
+			tsd.current_hour_date_count, 
+			tsd.current_minute_date_count, 
+			tsd.current_second_date_count
 		  );
 
-	printf("Years since date: %i\n", time_since_date.years_since_date);
-	printf("Months since date: %i\n", time_since_date.months_since_date);
-	printf("Weeks since date: %i\n", time_since_date.weeks_since_date);
-	printf("Days since date: %i\n", time_since_date.days_since_date);
-	printf("Hours since date: %i\n", time_since_date.hours_since_date);
-	printf("Minutes since date: %i\n", time_since_date.minutes_since_date);
-	printf("Seconds since date: %i\n", time_since_date.seconds_since_date);
+	printf("Years since date: %i\n", tsd.years_since_date);
+	printf("Months since date: %i\n", tsd.months_since_date);
+	printf("Weeks since date: %i\n", tsd.weeks_since_date);
+	printf("Days since date: %i\n", tsd.days_since_date);
+	printf("Hours since date: %i\n", tsd.hours_since_date);
+	printf("Minutes since date: %i\n", tsd.minutes_since_date);
+	printf("Seconds since date: %i\n", tsd.seconds_since_date);
 
 	return 0;
 }
